d89c7643ab92b66bfd9ac00710542aa242fb1d43
akhilyarlagadda <S545513@nwmissouri.edu>
2021-11-19 11:27:25 -0600

    added passport security

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var Canteen = require("./models/player");
 
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
@@ -62,6 +64,28 @@ app.use('/players', playersRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 app.use('/', resourceRouter);
+
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  }));
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+var Account =require('./models/account'); 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));

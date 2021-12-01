f85b9da33f8512579673b1ec5d61be9f68386860
akhilyarlagadda <S545513@nwmissouri.edu>
2021-11-30 23:32:21 -0600

    added buttons

--- routes/players.js
+++ routes/players.js
@@ -17,12 +17,12 @@ router.get('/', player_controlers.player_view_all_Page );
 router.get('/player/:id', player_controlers.player_detail); 
 
 //GET detail player page */ 
-router.get('/detail', player_controlers.player_view_one_Page); 
+router.get('/detail', secured, player_controlers.player_view_one_Page); 
 /* GET create player page */ 
 router.get('/create', player_controlers.player_create_Page);
 /* GET create update page */ 
 router.get('/update', secured, player_controlers.player_update_Page); 
 /* GET create player page */ 
-router.get('/delete', player_controlers.player_delete_Page); 
+router.get('/delete', secured, player_controlers.player_delete_Page); 
 
 module.exports = router;
